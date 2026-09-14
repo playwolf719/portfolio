9fa097186a4c3605e897d0e40c3db00eccf0eb67
登辰 <dengchenbo@dengchendeMacBook-Pro.local>
2026-09-14 17:21:51 +0800

    add for init

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.BASE_PATH || "/",
   optimizeDeps: {
     include: ["react", "react-dom/client"],
   },
